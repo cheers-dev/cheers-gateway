14832486a2b05c7d6cd3f5975ebeb016497c547e
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-03-28 20:12:08 +0800

    fix(UserController): add avatar setting and error handler

--- Sources/App/Controllers/UserController.swift
+++ Sources/App/Controllers/UserController.swift
@@ -11,11 +11,11 @@ import Vapor
 struct UserController: RouteCollection {
     func boot(routes: any Vapor.RoutesBuilder) throws {
         let user = routes.grouped("user")
-        
-        user.post("register", use: create)
+            
+        user.post("register", use: register)
     }
     
-    func create(req: Request) async throws -> User {
+    func register(req: Request) async throws -> Response {
         try User.Create.validate(content: req)
         let create = try req.content.decode(User.Create.self)
         
@@ -24,16 +24,37 @@ struct UserController: RouteCollection {
             throw Abort(.badRequest, reason: "Password not matched.")
         }
         
-        let user = try User(
-            account: create.account,
-            hashedPassword: Bcrypt.hash(create.password),
-            mail: create.mail,
-            name: create.name,
-            birth: create.birth,
-            avatar: create.avatar
-        )
+        let id = UUID()
         
-        try await user.save(on: req.db)
-        return user
+        var avatar: String?
+        if let avatarFile = create.avatar {
+            let imageType = avatarFile.filename.split(separator: ".")
+            avatar = "avatar/\(id.uuidString.lowercased()).\(imageType.last ?? "")"
+            do {
+                req.logger.log(level: .critical, "Writing image into \(avatar!)")
+                try await req.fileio.writeFile(avatarFile.data, at: "\(req.application.directory.publicDirectory)\(avatar!)")
+            } catch(let err) {
+                req.logger.error("\(err)")
+                throw Abort(.badRequest, reason: "\(err)")
+            }
+        }
+        
+        do {
+            let user = try User(
+                id: id,
+                account: create.account,
+                hashedPassword: Bcrypt.hash(create.password),
+                mail: create.mail,
+                name: create.name,
+                birth: create.birth,
+                avatar: avatar != nil ? URL(string: "http://localhost:8080/\(avatar!)") : nil
+            )
+            try await user.save(on: req.db)
+        } catch(let err) {
+            req.logger.error("\(err)")
+            throw Abort(.badRequest, reason: "\(err)")
+        }
+        
+        return Response(status: .created, body: "Account create successfully.")
     }
 }
