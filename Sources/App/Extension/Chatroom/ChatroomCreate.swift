08e99bcb11c9f19c0486393bbbb7187801d6e1ea
dongdong <93483694+dongdong867@users.noreply.github.com>
2024-05-05 16:00:31 +0800

    feat(ChatroomCreate): add chatroom create struct and validation

---
+++ Sources/App/Extension/Chatroom/ChatroomCreate.swift
@@ -0,0 +1,23 @@
+//
+//  ChatroomCreate.swift
+//  
+//
+//  Created by Dong on 2024/5/4.
+//
+
+import Fluent
+import Vapor
+
+extension Chatroom {
+    struct Create: Content {
+        var name: String
+        var userIds: [UUID]
+    }
+}
+
+extension Chatroom.Create: Validatable {
+    static func validations(_ validations: inout Vapor.Validations) {
+        validations.add("name", as: String.self, is: !.empty, required: true)
+        validations.add("userIds", as: [UUID].self, required: true)
+    }
+}
