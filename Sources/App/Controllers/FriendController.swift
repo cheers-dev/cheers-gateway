d2a05771db654a64ebbfead3b1b92eaccf55f9f0
dong <93483694+dongdong867@users.noreply.github.com>
2024-07-12 03:20:24 +0800

    feat(FriendController): create sendInvite func

--- Sources/App/Controllers/FriendController.swift
+++ Sources/App/Controllers/FriendController.swift
@@ -19,6 +19,47 @@ struct FriendController: RouteCollection {
     }
 
     func sendInvite(req: Request) async throws -> Response {
+        let user = try req.auth.require(User.self)
+        let data = try req.content.decode(Friend.Create.self)
+        
+        guard let addressee = try await User.query(on: req.db(.psql))
+            .filter(\.$id == data.addressee)
+            .first()
+        else { return .init(status: .badRequest) }
+        
+        let invitation = try await FriendInvitation
+            .query(on: req.db(.psql))
+            .group(.or) { group in
+                group
+                    .group { asRequestor in
+                        asRequestor
+                            .filter(\.$requestor.$id == user.id!)
+                            .filter(\.$addressee.$id == addressee.id!)
+                    }
+                    .group { asAddressee in
+                        asAddressee
+                            .filter(\.$requestor.$id == addressee.id!)
+                            .filter(\.$addressee.$id == user.id!)
+                    }
+            }.first()
+        
+        if invitation != nil {
+            if invitation!.status == .rejected {
+                invitation!.status = .pending
+                try await invitation!.update(on: req.db(.psql))
+                
+                return .init(status: .ok)
+            }
+            
+            return .init(status: .conflict)
+        }
+        
+        let createInvitation = try FriendInvitation(
+            requestor: user,
+            addressee: addressee
+        )
+        try await createInvitation.save(on: req.db(.psql))
+        
         return .init(status: .ok)
     }
 }
