e67bb7f05aa2b545d5e10fdfe860c5551879b4f6
dong <93483694+dongdong867@users.noreply.github.com>
2024-09-19 20:09:41 +0800

    fix: fix error on chatroom ID

--- Sources/App/Models/ChatroomParticipant/ChatroomParticipant+Extension.swift
+++ Sources/App/Models/ChatroomParticipant/ChatroomParticipant+Extension.swift
@@ -22,9 +22,10 @@ extension ChatroomParticipant {
 
         var chatroomInfos = [Chatroom.Info]()
         for userChatroom in userChatrooms {
+            try print(userChatroom.requireID())
             let lastMessage = try await Message
                 .query(on: req.db(.mongo))
-                .filter(\.$chatroomId == userChatroom.requireID())
+                .filter(\.$chatroomId == userChatroom.chatroom.requireID())
                 .sort(\.$createdAt, .descending)
                 .first()
 
