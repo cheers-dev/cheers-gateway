4c4e9bbf3b50d35eef29bee2de0b28ab7e81e29b
dong <93483694+dongdong867@users.noreply.github.com>
2024-10-21 14:42:01 +0800

    fix: set avatar required to false and remove redundant required setup

--- Sources/App/Extension/Chatroom/ChatroomCreate.swift
+++ Sources/App/Extension/Chatroom/ChatroomCreate.swift
@@ -1,12 +1,14 @@
 //
 //  ChatroomCreate.swift
-//  
+//
 //
 //  Created by Dong on 2024/5/4.
 //
 
 import Fluent
 import Vapor
+
+// MARK: - Chatroom.Create
 
 extension Chatroom {
     struct Create: Content {
@@ -16,10 +18,12 @@ extension Chatroom {
     }
 }
 
+// MARK: - Chatroom.Create + Validatable
+
 extension Chatroom.Create: Validatable {
     static func validations(_ validations: inout Vapor.Validations) {
-        validations.add("avatar", as: URL.self)
-        validations.add("name", as: String.self, is: !.empty, required: true)
-        validations.add("userIds", as: [UUID].self, required: true)
+        validations.add("avatar", as: URL.self, required: false)
+        validations.add("name", as: String.self, is: !.empty)
+        validations.add("userIds", as: [UUID].self)
     }
 }
