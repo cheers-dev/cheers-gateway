2276c8a8851ca52920299908cd6892ac8c016c5d
楊晏禎 <yangyenchen@yangyanzhendeMacBook-Pro.local>
2024-10-27 03:10:34 +0800

    add user preference api

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -31,6 +31,7 @@ public func configure(_ app: Application) async throws {
     app.migrations.add(ChatroomParticipantMigration(), to: .psql)
     app.migrations.add(FriendMigration(), to: .psql)
     app.migrations.add(FriendInvitationMigration(), to: .psql)
+    app.migrations.add(UserPreferenceMigration(), to: .psql)
     
     // mongo
     app.migrations.add(MessageMigration(), to: .mongo)
