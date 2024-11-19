0aa1f04ccf0274f9bcf43547f16a8f9f260e4291
dong <93483694+dongdong867@users.noreply.github.com>
2024-11-19 23:57:19 +0800

    feat: create UserPreference+Payload with func

---
+++ Sources/App/Extension/User/UserPreference.swift
@@ -0,0 +1,54 @@
+//
+//  UserPreference.swift
+//  cheers-gateway
+//
+//  Created by Dong on 11/14/24.
+//  Copyright © 2024 Dongdong867. All rights reserved.
+//
+
+import Vapor
+
+// MARK: - UserPreference.Payload
+
+extension UserPreference {
+    struct Payload: Codable {
+        let rankings: [Ranking]
+    }
+
+    struct Ranking: Codable {
+        let food: Category
+        let score: Int
+    }
+
+    static func toUserPreferenceModel(
+        _ payload: [Ranking],
+        userId: UUID
+    ) throws -> UserPreference {
+        var preferences = [UserPreference.Category: Int]()
+
+        for preference in payload {
+            preferences[preference.food] = preference.score
+        }
+
+        return UserPreference(userId: userId, preferences: preferences)
+    }
+
+    func updatingFromPayload(payload: [UserPreference.Ranking]) {
+        var preferences = [UserPreference.Category: Int]()
+
+        for preference in payload {
+            preferences[preference.food] = preference.score
+        }
+
+        self.createOrUpdateModel(preferences: preferences)
+    }
+}
+
+// MARK: - UserPreference.Payload + Validatable
+
+extension UserPreference.Payload: Validatable {
+    static func validations(_ validations: inout Validations) {
+        validations.add("food", as: String.self, is: !.empty)
+        validations.add("score", as: String.self, is: !.empty)
+    }
+}
