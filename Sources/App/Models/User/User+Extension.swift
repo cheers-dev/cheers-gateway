331717830ccb63e84048dc715ad228c2b1f0f1d7
dong <93483694+dongdong867@users.noreply.github.com>
2024-12-02 13:38:51 +0800

    feat: add user return for message

--- Sources/App/Models/User/User+Extension.swift
+++ Sources/App/Models/User/User+Extension.swift
@@ -7,7 +7,30 @@
 //
 
 import Fluent
+import Vapor
 
 extension User {
-    func empty() {}
+    static func getMessageWithSender(
+        _ req: Request,
+        message: Message
+    ) async throws -> Message.Get {
+        guard let user = try await User
+            .query(on: req.db(.psql))
+            .filter(\.$id == message.userId)
+            .first(),
+            let messageSendTime = message.createdAt
+        else { throw Abort(.noContent) }
+
+        return try Message.Get(
+            id: message.requireID(),
+            user: User.Get(
+                id: user.requireID(),
+                account: user.account,
+                mail: user.mail,
+                name: user.name
+            ),
+            content: message.content,
+            createAt: messageSendTime
+        )
+    }
 }
